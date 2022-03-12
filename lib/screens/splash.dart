c0137b837c097097e221d32b5322bc4c86362a74
mmsTtufan <ali.tufan@kc.com.tr>
2022-03-12 14:41:20 +0300

    firebase added

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({Key key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -11,6 +14,24 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    body:Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          GestureDetector(
+              onTap: (){
+                getData();
+              },
+              child: Text("data")),
+
+        ],
+      ),
+    ),
     );
+  }
+  getData() async {
+  await FirebaseFirestore.instance.collection("collection").add({
+    "num":Random().nextInt(100),
+  });
   }
 }
